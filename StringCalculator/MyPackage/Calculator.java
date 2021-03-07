5f6473e3eb8158dffb982d32149efe9ed1bc1eb0
rsurati <ras2692@gmail.com>
2021-03-07 12:15:49 +0530

    Stage-7_updated

--- StringCalculator/MyPackage/Calculator.java
+++ StringCalculator/MyPackage/Calculator.java
@@ -1,12 +1,12 @@
 package MyPackage;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
 	
-//Assuming Numbers are separated by comma(,) and New Line (\n)
-//Also combining for “//[delimiter]\n[numbers…]”
-//Handling Negative numbers also
-//For numbers greater than 1000 should be ignored
+//	Delimiters can be of
+//	any length with the following format: “//[delimiter]\n” for example: “//[***]\n1***2***3” should return 6
 
 	
 public int Add(String numbers) {
@@ -19,14 +19,14 @@ public int Add(String numbers) {
 		char del;
 		String delimiter="";
 		char[] ch = new char[numbers.length()]; 
-		
+	
  
 		del= numbers.charAt(3);
 	
 		for (int i = 3; i < numbers.length(); i++) { 
 			
 			System.out.println(numbers.charAt(i));
-			if(numbers.charAt(i)==']') {
+			if(numbers.charAt(i)==']'&&numbers.charAt(i+1)!='[') {
 				break;
 			}
 			count++;
@@ -37,13 +37,13 @@ public int Add(String numbers) {
 			
 			
 		if(numbers.isEmpty()) {
-			return 9;
+			return 0;
 		}
 		else {
 				
 				delimiter = Character.toString(numbers.charAt(3));
 				numbers = numbers.substring(count+5);
-				//System.out.println(numbers.toString());
+				System.out.println(numbers.toString());
 				
 				
 				for(int i=0;i<numbers.length();i++) {
@@ -53,15 +53,10 @@ public int Add(String numbers) {
 						char temp = numbers.charAt(i);
 						sum+=Integer.parseInt(Character.toString(temp));
 					}
-					
 				}
-				
-				
-			}
 			
-			
-		return sum;	
-		
+			}		
+		return sum;
 			
 		
 	}
